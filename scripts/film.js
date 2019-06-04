52668338d247c15eda6fc1e0e0568809d0bb87ed
Denali Chin <denalichin@Denalis-MacBook-Pro.local>
2019-06-04 00:13:05 -0700

    added video, but barely works

---
+++ scripts/film.js
@@ -0,0 +1,9 @@
+function playVideo(div){
+    console.log("hello");
+    console.log(div);
+//    let popup = div.querySelector(".popup")
+//    console.log(popup);
+    let popup = document.getElementById("popup");
+    popup.style.visibility = "visible";
+}
+
