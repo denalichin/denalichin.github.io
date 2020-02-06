e2a4232f5f61e12f4390b62b3755da1bdc4aa1b3
Denali Chin <denalichin@Denalis-MacBook-Pro.local>
2020-02-05 16:55:26 -0800

    project page now displays projects and descriptions

--- scripts/program.js
+++ scripts/program.js
@@ -1,3 +1,68 @@
 window.onload = function () {
     initNavBar();
+    populateLanguages();
+    populateProjects();
+}
+
+let projects = [
+    {
+        name: 'Concept Forum [2020]',
+        desc: 'Full-stack web application with Angular, Java, Spring boot, Mongo Atlas <br>\
+        Implemented user registration, REST, AJAX, nested comments',
+            
+    },
+    {
+        name: 'Bug Tracker [2019]',
+        desc: 'Full-stack web application with Angular, Java, Spring boot, Mongo Atlas <br>\
+        Implemented user registration, routing guards, REST, AJAX',
+    },
+    {
+        name: 'Machine Learning [2019]',
+        desc: 'Implemented with Python, Tensor Flow, Keras, NumPy <br>\
+        Trained a model to recognize articles of clothing',
+    },
+    {
+        name: 'This Portolio Website [2019]',
+        desc: 'Designed and developed responsive website with HTML, CSS, Javascript <br> \
+        Hosted website through github with custom domain name',
+    },
+]
+
+let languages = [
+    'Java',
+    'Javascript',
+    'HTML',
+    'CSS',
+    'Python',
+    'Matlab',
+    'Swift',
+    'C, C++, C#',
+    'SQL'
+]
+
+function populateLanguages(){
+    let lang = document.getElementById('skills');
+    for(lan of languages){
+        let p = document.createElement('p');
+        p.innerText = lan;
+        lang.appendChild(p);
+    }
+}
+
+function populateProjects(){
+    let proj_div = document.getElementById('projects');
+    for(p of projects){
+        let div = document.createElement('div');
+        div.className = "project_div";
+        let description = document.createElement('div');
+        let paragraph = document.createElement('p');
+
+        description.innerHTML = "<p class=\"description\">" + p.desc +"</p>";
+
+        div.appendChild(paragraph);
+        div.appendChild(description);
+
+        paragraph.innerText = p.name;
+        proj_div.appendChild(div);
+    }
 }
